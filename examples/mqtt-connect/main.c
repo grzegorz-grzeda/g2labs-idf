9b73ad61ff9682a3dc1fbff6a4edfd869fea1c8b
Grzegorz Grzęda <grzegorz.grzeda@g2labs.pl>
2024-09-18 23:39:11 +0200

    Added configuration storage to mqtt example

--- examples/mqtt-connect/main.c
+++ examples/mqtt-connect/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include "color-manipulation.h"
+#include "db.h"
 #include "g2l-hal-wifi.h"
 #include "g2l-log.h"
 #include "g2l-mqtt.h"
@@ -13,18 +14,30 @@
 
 #define TAG "mqtt-connect"
 
-#define WIFI_SSID "<SSID>"
-#define WIFI_PASSWORD "<PASSWORD>"
-#define MQTT_HOST "<BROKER URI"
-#define MQTT_PORT 1883
-#define MQTT_TOPIC_SUBSCRIBE "<TOPIC>"
+#define DB_NAMESPACE "cfg"
 
-#define WS28XX_LED_PIN (16)
-#define WS28XX_LED_COUNT (16)
-#define WS28XX_LED_SATURATION (100)
-#define WS28XX_LED_VALUE (10)
+#define DB_WIFI_SSID_KEY "wifi-ssid"
+#define DB_WIFI_PASSWORD_KEY "wifi-password"
+#define DB_MQTT_HOST_KEY "mqtt-host"
+#define DB_MQTT_PORT_KEY "mqtt-port"
+#define DB_MQTT_TOPIC_SUBSCRIBE_KEY "mqtt-subscribe"
+#define DB_LED_VALUE_KEY "led-value"
+#define DB_LED_SATURATION_KEY "led-saturation"
+#define DB_LED_PIN_KEY "led-pin"
+#define DB_LED_COUNT_KEY "led-count"
 
 static g2l_os_mutex_t* mutex = NULL;
+
+static char WIFI_SSID[32] = {0};
+static char WIFI_PASSWORD[32] = {0};
+static char MQTT_HOST[32] = {0};
+static int MQTT_PORT = 0;
+static char MQTT_TOPIC_SUBSCRIBE[32] = {0};
+
+static int WS28XX_LED_PIN = 0;
+static int WS28XX_LED_COUNT = 0;
+static int WS28XX_LED_VALUE = 0;
+static int WS28XX_LED_SATURATION = 0;
 
 static void on_wifi_event(void* ctx, g2l_hal_wifi_event_t event, void* data) {
     if (event == G2L_HAL_WIFI_STA_STARTED) {
@@ -88,7 +101,37 @@ static void on_mqtt_event(void* context,
     }
 }
 
+static void config_string(const char* key, char* value, size_t max_len) {
+    char buffer[32] = {0};
+    db_get(DB_NAMESPACE, key, buffer, sizeof(buffer));
+    if (strlen(buffer) > 0) {
+        strncpy(value, buffer, max_len);
+    }
+}
+
+static void config_int(const char* key, int* value) {
+    char buffer[32] = {0};
+    db_get(DB_NAMESPACE, key, buffer, sizeof(buffer));
+    *value = atoi(buffer);
+}
+
+static void load_configuration() {
+    db_init();
+
+    config_string(DB_WIFI_SSID_KEY, WIFI_SSID, sizeof(WIFI_SSID));
+    config_string(DB_WIFI_PASSWORD_KEY, WIFI_PASSWORD, sizeof(WIFI_PASSWORD));
+    config_string(DB_MQTT_HOST_KEY, MQTT_HOST, sizeof(MQTT_HOST));
+    config_int(DB_MQTT_PORT_KEY, &MQTT_PORT);
+    config_string(DB_MQTT_TOPIC_SUBSCRIBE_KEY, MQTT_TOPIC_SUBSCRIBE,
+                  sizeof(MQTT_TOPIC_SUBSCRIBE));
+    config_int(DB_LED_VALUE_KEY, &WS28XX_LED_VALUE);
+    config_int(DB_LED_SATURATION_KEY, &WS28XX_LED_SATURATION);
+    config_int(DB_LED_PIN_KEY, &WS28XX_LED_PIN);
+    config_int(DB_LED_COUNT_KEY, &WS28XX_LED_COUNT);
+}
+
 int main(int argc, char** argv) {
+    load_configuration();
     g2l_hal_ws28xx_initialize(WS28XX_LED_PIN, WS28XX_LED_COUNT);
 
     g2l_hal_wifi_initialize();
